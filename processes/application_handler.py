b973a25bc2527482746692b2213a15936a1e194c
Sebastian <ahss@aarhus.dk>
2025-09-24 12:39:23 +0200

    Implement functions to close application

--- processes/application_handler.py
+++ processes/application_handler.py
@@ -1,6 +1,8 @@
 """Module for handling application startup, and close"""
 
 import logging
+import subprocess as sp
+from subprocess import CalledProcessError
 
 from mbu_dev_shared_components.database.connection import RPAConnection
 from mbu_dev_shared_components.solteqtand.application import SolteqTandApp
@@ -39,18 +41,35 @@ def startup():
 def soft_close():
     """Function for closing applications softly"""
     logger.info("Closing applications softly...")
+    solteq_app = get_app()
+    try:
+        solteq_app.close_solteq_tand()
+        logger.info("Closed application softly")
+    except Exception as e:
+        logger.error("Could not close application softly")
 
 
 def hard_close():
     """Function for closing applications hard"""
     logger.info("Closing applications hard...")
+    list_processes = ["wmic", "process", "get", "description"]
+    if "TMTand.exe" in sp.check_output(list_processes).strip().decode():
+        try:
+            kill_msg = sp.check_output(["taskkill", "/f", "/im", "TMTand.exe"])
+            logger.info(kill_msg)
+        except CalledProcessError as e:
+            logger.error(
+                f"TMTand.exe found in subprocesses, but error while killing it: {e}"
+            )
 
 
 def close():
     """Function for closing applications softly or hardly if necessary"""
-    try:
+    solteq_app = get_app()
+    if solteq_app:
         soft_close()
-    except Exception:
+    solteq_app = get_app()
+    if solteq_app:
         hard_close()
 
 
